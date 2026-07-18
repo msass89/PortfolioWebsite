9b9d33b2d400d5036a8d5327a67217571922dddf
msass89 <maikesass89@gmail.com>
2026-07-18 21:51:58 +0200

    Added dynamical project creation by JS

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
   emailjs.init("FbcccHWRjmInOlwSO");
 })();
 
+class Project {
+  constructor(projectClass, imageSource, imageAlt, projectName, projectDescription, projectLink, lastUpdate) {
+    this.projectClass = projectClass;
+    this.projectName = projectName;
+    this.imageSource = imageSource;
+    this.imageAlt = imageAlt;
+    this.imgClass = "lightbox-img";
+    this.projectDescription = projectDescription;
+    this.projectLink = projectLink;
+    this.lastUpdate = lastUpdate;
+  }
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -10,11 +23,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const burger = document.getElementById("burger");
   const nav = document.querySelector("nav");
   const navLinks = document.querySelectorAll(".nav a");
+  const projectGallery = document.getElementById("project-gallery");
   const checkboxes = document.querySelectorAll(".filters input");
-  const items = document.querySelectorAll(".project-gallery .item");
   const lightbox = document.getElementById("lightbox");
   const lightboxImage = document.getElementById("lightbox-image");
+  const lightboxText = document.getElementById("lightbox-text");
   const closeButton = document.getElementById("lightbox-close");
+  
+  // set up the project items in the gallery
+  const projects = [
+    new Project(
+      "web", 
+      "img/self_service_portal.png", 
+      "Screenshot of Self-Service-Portal", 
+      "Self-Service-Portal", 
+      "A multi-tenancy SaaS fullstack-project build with ASP.NET Razor Pages. Currently in development.", 
+      "https://github.com/msass89/Self-Service-Portal", 
+      "2024-05-18"),
+    new Project("web", 
+      "img/InventoryManagementApi.png", 
+      "Screenshot of Inventory and Order Management API", 
+      "Inventory and Order Management API", 
+      "A RESTful API for managing inventory items and orders using Asp.Net and Swagger. Currently in development.", 
+      "https://github.com/msass89/Inventory-Management-API", 
+      "2024-05-30"),
+    new Project("game", 
+      "img/roots_of_revival_screenshot.png", 
+      "Screenshot of Roots of Revival", 
+      "Roots of Revival", 
+      "An indie game I am currently developing with Unity, C# and Blender.", 
+      "https://github.com/msass89/Roots-of-Revival", 
+      "2025-12-15")
+  ];
 
   // Toggle navigation menu on burger click
   burger.addEventListener("click", () => {
@@ -28,32 +68,40 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Send contact form using EmailJS
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+  projects.forEach(project => {
+    const projectElement = document.createElement("figure");
+    projectElement.classList.add("item", project.projectClass);
 
-    emailjs.sendForm("service_swj6m08", "template_of9jjc8", this)
-      .then(function () {
-        alert("Nachricht wurde erfolgreich gesendet!");
-        form.reset();
-      })
-      .catch(function (error) {
-        alert("Fehler beim Senden: " + error.text);
-    });
+    const img = document.createElement("img");
+    img.src = project.imageSource;
+    img.alt = project.imageAlt;
+    img.setAttribute("role", "image");
+    img.classList.add(project.imgClass);
+    projectElement.appendChild(img);
 
-    emailjs.sendForm("service_swj6m08", "template_18chji6", this)
-        .then(function () {
-            alert("Auto-Response wurde erfolgreich gesendet!");
-        })
-        .catch(function (error) {
-            alert("Fehler beim Senden der Auto-Response: " + error.text);
-        });
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerHTML = `<h3><b>${project.projectName}</b></h3>`;
+    figcaption.setAttribute("role", "heading");
+    projectElement.appendChild(figcaption);
+
+    const details = document.createElement("p");
+    details.innerHTML = `${project.projectDescription} <br><br>See more details on <a href="${project.projectLink}" target="_blank" rel="noopener" role="link">GitHub</a>.`;
+    details.setAttribute("role", "contentinfo");
+    projectElement.appendChild(details);
+
+    const lastUpdate = document.createElement("p");
+    lastUpdate.innerHTML = `<b>Last Update: </b><time datetime="${project.lastUpdate}"><i>${project.lastUpdate}</i></time>`;
+    lastUpdate.setAttribute("role", "contentinfo");
+    projectElement.appendChild(lastUpdate);
+
+    projectGallery.appendChild(projectElement);
   });
 
   // Lightbox-Function for project images
   document.querySelectorAll(".lightbox-img").forEach(img => {
   img.addEventListener("click", () => {
       lightboxImage.src = img.src;
+      lightboxText.textContent = img.getAttribute("data-text");
       lightbox.classList.add("active");
     });
   });
@@ -68,7 +116,8 @@ document.addEventListener("DOMContentLoaded", function () {
     if (event.target === lightbox) lightbox.classList.remove("active");
   });
 
-    // Filter project items based on selected checkboxes
+  // Filter project items based on selected checkboxes
+  items = projectGallery.querySelectorAll(".item");
   checkboxes.forEach(checkbox => {
     checkbox.addEventListener("change", () => {
 
@@ -109,6 +158,28 @@ document.addEventListener("DOMContentLoaded", function () {
       item.classList.toggle("hide", !match);
     });
   }
+
+  // Send contact form using EmailJS
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs.sendForm("service_swj6m08", "template_of9jjc8", this)
+      .then(function () {
+        alert("Nachricht wurde erfolgreich gesendet!");
+        form.reset();
+      })
+      .catch(function (error) {
+        alert("Fehler beim Senden: " + error.text);
+    });
+
+    emailjs.sendForm("service_swj6m08", "template_18chji6", this)
+        .then(function () {
+            alert("Auto-Response wurde erfolgreich gesendet!");
+        })
+        .catch(function (error) {
+            alert("Fehler beim Senden der Auto-Response: " + error.text);
+        });
+  });
 
 });
 
